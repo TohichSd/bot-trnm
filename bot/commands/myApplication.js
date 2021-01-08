8eba12f64f909edf19997e6fed720839d828abc7
TohichSd <anton.sur17@gmail.com>
2021-01-08 22:45:57 +0300

    Embed myApplication answer

--- bot/commands/myApplication.js
+++ bot/commands/myApplication.js
@@ -1,6 +1,9 @@
 import applicationsManager from "../helpers/applicationsManager.js"
 import winston_logger from "../../modules/logger/index.js"
 import dfname from "../../utils/__dfname.js"
+import {
+    MessageEmbed
+} from "discord.js"
 
 const logger = new winston_logger(dfname.dirfilename(import.meta.url))
 
@@ -17,7 +20,16 @@ function main(msg) {
                     resolve()
                     return
                 }
-                msg.reply(`Ваша заявка:\nСсылка на steam: ${row['link']}\nУровень в игре: ${row['level']}\nВозраст: ${row['age']}\nНаличие микрофона: ${row['micro']}`)
+                let embed = new MessageEmbed()
+                    .setThumbnail("https://i.ibb.co/1Q7pQ94/podacha.png")
+                    .setTitle(`**Заявка участника ${msg.member.displayName}**`)
+                    .addField(":link: Ссылка на steam:", "\n"+row['link'])
+                    .addField(":video_game: Уровень в игре:", "\n"+row['level'])
+                    .addField(":man_mage: Возраст:", "\n"+row['age'])
+                    .addField(":microphone2: Наличие микрофона:", "\n"+row['micro'])
+                    .setColor("#4287f5")
+                msg.reply(embed)
+                // msg.reply(`Ваша заявка:\nСсылка на steam: ${row['link']}\nУровень в игре: ${row['level']}\nВозраст: ${row['age']}\nНаличие микрофона: ${row['micro']}`)
                 resolve()
             })
         } catch (err) {
