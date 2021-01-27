674c8ba2f36b10f7d09ebe6290fa79bb260233c6
TohichSd <anton.sur17@gmail.com>
2021-01-27 22:32:01 +0300

    Bot status added

--- bot/bot.js
+++ bot/bot.js
@@ -20,7 +20,7 @@ client.login(env.DSTOKEN)
 client.on('ready', () => {
     logger.info("Discord client ready")
     server(client)
-    client.user.setStatus("Управление: https://bit.ly/3cbHYr9")
+    client.user.setActivity("!help")
 })
 client.on('guildCreate', (newGuild) => {
     helpers.init.run(newGuild)
