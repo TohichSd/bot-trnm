eee59efd2f874a9d03c710f8825d85688bf6ca02
TohichSd <anton.sur17@gmail.com>
2021-04-25 21:11:25 +0300

    Reformat and optimization

--- server/server.js
+++ server/server.js
@@ -9,46 +9,72 @@
  * D_CLIENT_SECRET - client secret из Discord
  */
 
-import express from 'express'
-import {env} from 'process'
-import session from 'express-session'
-import bodyParser from 'body-parser'
-import pug from 'pug'
-import _MongoDBStore from 'connect-mongodb-session'
+import express from "express"
+import { env } from "process"
+import session from "express-session"
+import bodyParser from "body-parser"
+import pug from "pug"
+import _MongoDBStore from "connect-mongodb-session"
 import router from "./routes.js"
-import values from './config/values.config.js'
+import values from "./config/values.config.js"
+import { sendReport } from "../bot/bot.js"
 
 const MongoDBStore = _MongoDBStore(session)
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.set("view engine", "pug")
 app.use(express.static("server/public"))
 
-app.use(session({
+app.use(
+  session({
     secret: env.SESSION_SECRET,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
     },
     store: new MongoDBStore({
-        uri: env.CONNECTION_STRING,
-        collection: 'mySessions'
+      uri: env.CONNECTION_STRING,
+      collection: "mySessions",
     }),
     resave: false,
-    saveUninitialized: false
-}))
+    saveUninitialized: false,
+  })
+)
 
 // Вызов роутера
 app.use(router)
 
 // 404
 app.use((req, res) => {
-    const err = new Error(values.error[404])
-    err.status = 404
-    const compiledPage = pug.renderFile("server/views/err.pug", {code: err.status, text: err.message})
-    res.status(err.status)
-    res.send(compiledPage)
+  const err = new Error(values.error[404])
+  err.status = 404
+  const compiledPage = pug.renderFile("server/views/err.pug", {
+    code: err.status,
+    text: err.message,
+  })
+  res.status(err.status)
+  res.send(compiledPage)
+})
+
+// Обработчик ошибок
+// eslint-disable-next-line no-unused-vars
+router.use((err, req, res, next) => {
+  req.body = null
+  if (err.code && err.text) {
+    res.send(
+      pug.renderFile("server/views/err.pug", { code: err.code, text: err.text })
+    )
+  } else {
+    res.status(500)
+    sendReport(err.stack)
+    res.send(
+      pug.renderFile("server/views/err.pug", {
+        code: 500,
+        text: "Произошла непредвиденная ошибка.",
+      })
+    )
+  }
 })
 
 export default app
