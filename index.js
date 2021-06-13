03caba11a9fcc0379a63a91a767626909576c863
TohichSd <anton.sur17@gmail.com>
2021-06-13 12:45:01 +0300

    Env update

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 import { env } from 'process'
 import mongoose from 'mongoose'
+import dotenv from 'dotenv'
 import { sendReport, start } from './src/bot.js'
 import { app } from './src/server/server.js'
 
+dotenv.config()
 start().catch(sendReport)
-mongoose.connect(env.CONNECTION_STRING, { useUnifiedTopology: true, useNewUrlParser: true})
+mongoose
+  .connect(env.CONNECTION_STRING, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
   .then(() => {
-    if(env.NODE_ENV === 'development') console.log('Connected to db')
+    if (env.NODE_ENV === 'development') console.log('Connected to db')
   })
   .catch(sendReport)
 
 app.listen(env.PORT)
-
