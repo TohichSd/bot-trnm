2a134e112f17fa63db7c77d39ab2475f2de70fb5
TohichSd <anton.sur17@gmail.com>
2021-10-04 20:07:59 +0300

    Исправлена ошибка при таймауте и отмене заявки

--- src/commands/newApplication.js
+++ src/commands/newApplication.js
@@ -4,9 +4,13 @@ import { sendReport } from '../bot.js'
 import Interview from '../controllers/Interview.js'
 import { ApplicationModel, GuildModel } from '../db/dbModels.js'
 
+/**
+ * @param {module:"discord.js".Message} message
+ * @return {Promise<void>}
+ */
 const main = async message => {
   const guildDB = await GuildModel.findOneByGuildID(message.guild.id)
-  if(message.channel.id !== guildDB.new_app_channel) {
+  if (message.channel.id !== guildDB.new_app_channel) {
     const errMgs = await message.channel.send(
       `<@${message.author.id}>, Пожалуйста, заполняйте заявку только в канале <#${guildDB.new_app_channel}>`
     )
@@ -21,18 +25,27 @@ const main = async message => {
       .then(data => data.toString())
       .catch(sendReport)
   )
-  if(questions === undefined) return
+  if (questions === undefined) return
   const interview = new Interview(
     questions,
     message.channel,
     message.author.id,
     'Создание заявки'
   )
-  const answers = await interview.start().catch(err => {
-    if (err._array === null) message.reply('Время на заполнение заявки вышло.')
-    else if (err.message === 'Stop') message.reply('Отменено.')
-    else sendReport(err.stack)
-  })
+  let answers
+  try {
+    answers = await interview.start()
+  } catch (err) {
+    if (err._array === null) {
+      message.reply('Время на заполнение заявки вышло.')
+      return
+    } 
+    if (err.message === 'Stop') {
+      message.reply('Отмена')
+      return
+    } 
+    throw err.stack
+  }
   await cachegoose.clearCache(`application/${message.member.id}`)
   await ApplicationModel.updateOne(
     { id: message.member.id },
