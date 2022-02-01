2f49d126a1e2f7fe49dac4bdac87d57acf9e0d44
TohichSd <anton.sur17@gmail.com>
2022-02-01 21:53:17 +0300

    Изменена картика войны кланов

--- src/config/clan_war_message.js
+++ src/config/clan_war_message.js
@@ -1,6 +1,6 @@
 export default {
   author: 'Клановая-война',
   image:
-    'https://cdn.discordapp.com/attachments/828197105415618560/894614029962711100/Armello----furry-3396311.jpeg',
+    'https://cdn.discordapp.com/attachments/828197105415618560/938122886177230918/02_Rangers.jpg',
   color: '#8127fc',
 }
