1cc8adf4ed1b8a2be865a2084bc768c87b39d058
TohichSd <anton.sur17@gmail.com>
2022-03-03 21:15:45 +0300

    Изменена картинка войны

--- src/config/clan_war_message.js
+++ src/config/clan_war_message.js
@@ -1,6 +1,6 @@
 export default {
   author: 'Клановая-война',
   image:
-    'https://cdn.discordapp.com/attachments/828197105415618560/938122886177230918/02_Rangers.jpg',
+    'https://cdn.discordapp.com/attachments/828197105415618560/938122887288746024/jack-crosby-palacewest09.gif',
   color: '#8127fc',
 }
