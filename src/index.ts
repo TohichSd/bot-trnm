25995af75dba353810bbeeb9664d93fe7a10a4a3
TohichSd <anton.sur17@gmail.com>
2022-09-04 22:48:19 +0300

    Https server

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,8 @@ import { Config } from './config/BotConfig'
 import Logger from './classes/Logger'
 import Server from './server/Server'
 
+import { readFileSync } from 'fs'
+
 ;(async () => {
     await mongoose
         .connect(env.CONNECTION_STRING)
@@ -20,14 +22,19 @@ import Server from './server/Server'
     try {
         const server = Server.getInstance()
         await server.loadRoutes(join(__dirname, '/server/routes'))
-        server.startHTTP(parseInt(env.PORT))
+        if (env.SSL_CERT_PATH && env.SSL_KEY_PATH) {
+            server.startHTTPS(
+                443,
+                readFileSync(env.SSL_KEY_PATH).toString(),
+                readFileSync(env.SSL_CERT_PATH).toString()
+            )
+        } else server.startHTTP(parseInt(env.PORT))
 
         const commands = await CommandsManager.load(join(__dirname, '/discord/commands'))
         const bot = Bot.getInstance()
         await bot.init(env.D_TOKEN, commands, Config.BotConfig.username)
         console.log('Discord client ready')
-    }
-    catch (e) {
+    } catch (e) {
         Logger.error(e)
     }
 })()
